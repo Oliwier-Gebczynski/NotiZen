c84b9cac11babae97bcf6f66554ebeb2e4ba1750
Oliwier-Gebczynski <oliwiergebczynski@gmail.com>
2021-02-17 14:37:13 +0100

    main page update

--- script/nav.js
+++ script/nav.js
@@ -1,15 +1,28 @@
 const notesBtn = document.querySelector("#notes");
 const toDoBtn = document.querySelector("#toDo");
 const toDoSection = document.querySelector("#listSection");
+const home = document.querySelector("#home");
 const note = document.querySelector(".note");
+const footer = document.querySelector("#footer");
+const homeBtn = document.querySelector("#homeBtn");
 
 notesBtn.addEventListener("click", function () {
     toDoSection.style.display = "none";
     note.style.display = "flex";
+    home.style.display = "none";
+    footer.style.display = "none";
 });
 
 toDoBtn.addEventListener("click", function () {
     toDoSection.style.display = "flex";
     note.style.display = "none";
-
+    home.style.display = "none"
+    footer.style.display = "none";
 });
+
+homeBtn.addEventListener("click", function(){
+    toDoSection.style.display = "none";
+    note.style.display = "none";
+    home.style.display = "flex";
+    footer.style.display = "block";
+})
