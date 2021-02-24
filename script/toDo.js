67a88a729043bc289b935bab118113a2b3dbac47
Oliwier-Gebczynski <oliwiergebczynski@gmail.com>
2021-02-24 13:55:23 +0100

    toDo upgrade

--- script/toDo.js
+++ script/toDo.js
@@ -4,26 +4,33 @@ submitBtn.addEventListener('click', function (event) {
     event.preventDefault();
     const input = document.querySelector('#input').value;
 
-    const listEl = document.createElement('div');
-    listEl.classList.add("catalogue__element");
+    if(input==''){
+        alert("Wpisz dane w pole tekstowe!");
+    }
+    else{
+        const listEl = document.createElement('div');
+        listEl.classList.add("catalogue__element");
 
-    const content = document.createElement('h3');
-    content.classList.add("catalogue__element-content");
-    content.innerText = `${input}`;
+        const content = document.createElement('h3');
+        content.classList.add("catalogue__element-content");
+        content.innerText = `${input}`;
 
-    const deleteBtn = document.createElement('button');
-    deleteBtn.classList.add("catalogue__element-delete");
-    deleteBtn.innerText = `x`;
+        const deleteBtn = document.createElement('button');
+        deleteBtn.classList.add("catalogue__element-delete");
+        deleteBtn.innerText = `x`;
 
-    listEl.appendChild(content);
-    listEl.appendChild(deleteBtn);
+        listEl.appendChild(content);
+        listEl.appendChild(deleteBtn);
 
-    deleteBtn.addEventListener('click', function () {
-        listEl.remove();
-    });
+        deleteBtn.addEventListener('click', function () {
+            listEl.remove();
+        });
 
-    const inputReset = document.querySelector('#input').value = '';
+        const inputReset = document.querySelector('#input').value = '';
 
-    document.querySelector('.catalogue').appendChild(listEl);
+        document.querySelector('.catalogue').appendChild(listEl);
+    }
+
+    
 
 });
