0fde26a4c8faea34aea290cc75027cfe3953f31b
Oliwier-Gebczynski <oliwiergebczynski@gmail.com>
2021-02-26 22:52:39 +0100

    toDo upgrade

--- script/toDo.js
+++ script/toDo.js
@@ -3,6 +3,7 @@ const submitBtn = document.querySelector('#submit');
 submitBtn.addEventListener('click', function (event) {
     event.preventDefault();
     const input = document.querySelector('#input').value;
+    const catalogue = document.querySelector('.catalogue');
 
     if(input==''){
         alert("Wpisz dane w pole tekstowe!");
@@ -15,20 +16,33 @@ submitBtn.addEventListener('click', function (event) {
         content.classList.add("catalogue__element-content");
         content.innerText = `${input}`;
 
+        const doneBtn =  document.createElement('button');
+        doneBtn.classList.add("catalogue__element-done");
+        doneBtn.innerText = '✔️';
+
         const deleteBtn = document.createElement('button');
         deleteBtn.classList.add("catalogue__element-delete");
         deleteBtn.innerText = `x`;
 
         listEl.appendChild(content);
+        listEl.appendChild(doneBtn);
         listEl.appendChild(deleteBtn);
+        
 
         deleteBtn.addEventListener('click', function () {
             listEl.remove();
         });
 
+        doneBtn.addEventListener('click', function(){
+            listEl.style.borderColor = 'rgb(4, 126, 0)';
+            content.style.color = 'rgb(4, 126, 0)';
+            catalogue.append(listEl);
+            this.remove();
+        })
+
         const inputReset = document.querySelector('#input').value = '';
 
-        document.querySelector('.catalogue').appendChild(listEl);
+        catalogue.appendChild(listEl);
     }
 
     
