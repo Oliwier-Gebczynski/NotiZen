32ba74ab64e60c8e753df6f5e0f9a63c68eed59c
Oliwier-Gebczynski <oliwiergebczynski@gmail.com>
2021-02-28 16:16:34 +0100

    dark theme upgrade

--- script/theme.js
+++ script/theme.js
@@ -1,6 +1,7 @@
 const containerDiv = document.querySelector("*");
 const switchBtn = document.querySelector("#switchBtn");
 
+
 switchBtn.addEventListener("click", function(){
     containerDiv.classList.toggle("dark-theme");
 })
