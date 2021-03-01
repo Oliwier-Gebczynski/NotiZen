e83e5bb8b836494afb9a3ab4f6bf01b069dc8a4d
Oliwier-Gebczynski <oliwiergebczynski@gmail.com>
2021-03-01 23:39:07 +0100

    start with timer (js)

---
+++ script/pomodoro.js
@@ -0,0 +1,10 @@
+const startBtn = document.querySelector('#startBtn');
+
+startBtn.addEventListener("click", function(){
+    const hours = parseInt(document.querySelector('#hours').value);
+    const minutes = parseInt(document.querySelector('#minutes').value);
+    const seconds = parseInt(document.querySelector('#seconds').value);
+
+    const form = document.querySelector('.form');
+
+})
